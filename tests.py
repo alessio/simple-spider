18e22ffc8b992ff0827e92496bd473ed051c5971
Alessio Treglia <quadrispro@ubuntu.com>
2015-11-13 19:53:22 +0000

    Draft tests

--- tests.py
+++ tests.py
@@ -1,5 +1,23 @@
 from unittest import TestCase
 
-class PlainPageTest(TestCase):
-    pass
 
+class PlainPageTest(TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        pass
+
+    def test_start_page_crawled(self):
+        pass
+
+    def test_page_crawled(self):
+        pass
+
+    def test_links_within_allowed_domain(self):
+        pass
+
+    def test_external_outside_domain(self):
+        pass
+
+    def test_external_not_visited(self):
+        pass
