837a4f95e6935bf1afe0b603c2223b3a2942e6bd
Alessio Treglia <quadrispro@ubuntu.com>
2015-11-13 19:29:17 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="simple-spider",
+    version="0.0.1",
+    author="Alessio Treglia",
+    author_email="quadrispro@ubuntu.com",
+    install_requires=[
+        "Scrapy>=1.0.3",
+    ],
+    scripts=["crawler.py"],
+)
