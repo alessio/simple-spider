5de8db3f5b6d69ee0c9b93cca9f035ba2703ae49
Alessio Treglia <quadrispro@ubuntu.com>
2015-11-13 20:31:47 +0000

    Implement tests

--- tests.py
+++ tests.py
@@ -1,23 +1,58 @@
+from collections import defaultdict
+from functools import partial
 from unittest import TestCase
+from urlparse import urlparse
+
+from scrapy.crawler import CrawlerProcess
+
+import crawler
 
 
-class PlainPageTest(TestCase):
+class WebsiteStandardTest(TestCase):
 
     @classmethod
     def setUpClass(self):
-        pass
+        self.results = defaultdict(partial(defaultdict, set))
+        self.domain = "thisisglow.com"
+        self.url = "http://thisisglow.com/"
+
+        settings = {"LOG_ENABLED": False}
+        process = CrawlerProcess(settings)
+        process.crawl(
+            crawler.WebSpider,
+            results=self.results,
+            allowed_domains=[self.domain],
+            start_urls=[self.url],
+        )
+        process.start()
 
     def test_start_page_crawled(self):
-        pass
+        self.assertIn(
+            "http://thisisglow.com/",
+            self.results,
+        )
 
     def test_page_crawled(self):
-        pass
+        self.assertIn(
+            "http://thisisglow.com/contact-us/",
+            self.results["http://thisisglow.com/"]["links"],
+        )
+        self.assertIn(
+            "http://thisisglow.com/contact-us/",
+            self.results,
+        )
 
     def test_links_within_allowed_domain(self):
-        pass
+        for child in self.results["http://thisisglow.com/"]["links"]:
+            self.assertEqual(urlparse(child).netloc, self.domain)
 
     def test_external_outside_domain(self):
-        pass
+        for link in self.pages["http://thisisglow.com/"]["external"]:
+            self.assertNotEqual(urlparse(link).netloc, self.domain)
 
     def test_external_not_visited(self):
-        pass
+        page = "http://thisisglow.com/managed-services/"
+        external_page = "http://twitter.com/thisisglow"
+
+        self.assertIn(external_page, self.pages[page]["external"])
+        self.assertNotIn(external_page, self.pages)
